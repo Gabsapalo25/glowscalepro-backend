1fd6762ae1cda2b6151bef49f2cd047a155399e8
Seu Nome <seu@email.com>
2025-08-30 14:26:14 +0100

    Finalizado integração com dois eBooks para primebiome

--- config/quizzesConfig.js
+++ config/quizzesConfig.js
@@ -1,4 +1,5 @@
-// config/quizzesConfig.js
+import { templates } from "../services/templates/templates.js";
+
 export const quizzesConfig = {
   tokmate: {
     leadTag: 10,
@@ -6,50 +7,50 @@ export const quizzesConfig = {
     ctaText: "Start Growing on TikTok",
     subject: "Your TokMate Quiz Results",
     ebookUrl: "https://drive.google.com/file/d/1L9tg7rkanAWZEgxCds_u3equ6rG2TgYm/view?usp=drive_link",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.tokmate
   },
   primebiome: {
     leadTag: 11,
     ctaColor: "#10b981",
-    ctaText: "Discover Your PrimeBiome Solution",
+    ctaText: "Discover Your Gut Health Solution",
     subject: "Your PrimeBiome Quiz Results",
     ebookUrl: "https://drive.google.com/file/d/16IwQhvWPM3yxEt-5ToYmJYmf1wBOYGXq/view?usp=sharing", // Your Main Guide
     ebookUrl2: "https://drive.google.com/file/d/1-p_1JnsA8cSRG8IMwpbQ-amLP_tyFbJ7/view?usp=drive_link", // Skin Vitality Checklist
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.primebiome
   },
   prodentim: {
     leadTag: 12,
     ctaColor: "#06b6d4",
     ctaText: "Unlock Your Oral Health Secrets",
     subject: "Your ProDentim Quiz Results",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.prodentim
   },
   nervovive: {
     leadTag: 1,
     ctaColor: "#6366f1",
     ctaText: "Find Your Calm",
     subject: "Your NervoVive Quiz Results",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.nervovive
   },
   totalcontrol24: {
     leadTag: 15,
     ctaColor: "#f97316",
     ctaText: "Gain Back Control",
     subject: "Your TotalControl24 Quiz Results",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.totalcontrol24
   },
   glucoshield: {
     leadTag: 13,
     ctaColor: "#ef4444",
     ctaText: "Stabilize Your Glucose",
     subject: "Your GlucoShield Quiz Results",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.glucoshield
   },
   prostadine: {
     leadTag: 14,
     ctaColor: "#84cc16",
     ctaText: "Support Your Prostate Health",
     subject: "Your Prostadine Quiz Results",
-    generateEmailHtml: null // Placeholder, a ser definido em templates.js
+    generateEmailHtml: templates.prostadine
   }
 };
